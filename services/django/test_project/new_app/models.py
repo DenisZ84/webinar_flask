16fcdf6fac1b473dfe51376d0937e21f86afaaf4
Denis Z <zamyatin.d@gmail.com>
2023-12-02 11:37:17 +0300

    changed new code

--- services/django/test_project/new_app/models.py
+++ services/django/test_project/new_app/models.py
@@ -3,4 +3,5 @@ from django.db import models
 # Create your models here.
 class MyApp(models.Model):
     name = models.CharField(max_length=123)
-    
+    name2 = models.CharField(max_length=123)
+
